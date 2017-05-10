224cc9a7e2b4d9ad30417b95242afef994f6d7b3
43081j <43081j@localhost>
2017-05-10 20:37:34 +0100

    test for fzf

--- zsh/.zsh/keys.zsh
+++ zsh/.zsh/keys.zsh
@@ -1,5 +1,10 @@
-bindkey -M viins '^R' fzf-history-widget
-bindkey -M vicmd '^R' fzf-history-widget
+if type "fzf" > /dev/null; then
+	bindkey -M viins '^R' fzf-history-widget
+	bindkey -M vicmd '^R' fzf-history-widget
+else
+	bindkey -M viins '^R' history-incremental-search-backward
+	bindkey -M vicmd '^R' history-incremental-search-backward
+fi
 bindkey -M viins "^[[3~" delete-char
 bindkey -M viins "^[[1~" vi-beginning-of-line
 bindkey -M viins "^[[4~" vi-end-of-line
