408e631f4c8360ba7027e25f125879a6d2ee8b20
43081j <43081j@users.noreply.github.com>
2017-05-17 16:36:34 +0100

    add extra fzf bindings

--- zsh/.zsh/keys.zsh
+++ zsh/.zsh/keys.zsh
@@ -1,6 +1,9 @@
 if type "fzf" > /dev/null; then
 	bindkey -M viins '^R' fzf-history-widget
 	bindkey -M vicmd '^R' fzf-history-widget
+	bindkey -M vicmd '^T' fzf-file-widget
+	bindkey -M viins '^T' fzf-file-widget
+	bindkey -M vicmd '\ec' fzf-cd-widget
 else
 	bindkey -M viins '^R' history-incremental-search-backward
 	bindkey -M vicmd '^R' history-incremental-search-backward
--- zsh/.zsh/options.zsh
+++ zsh/.zsh/options.zsh
@@ -2,3 +2,9 @@ setopt appendhistory extendedglob
 unsetopt beep
 
 PURE_GIT_PULL=0
+
+if type "rg" > /dev/null; then
+	FZF_DEFAULT_COMMAND='(git ls-tree -r --name-only HEAD || rg -g "")'
+else
+	FZF_DEFAULT_COMMAND='(git ls-tree -r --name-only HEAD || ag -g "")'
+fi
