c97ab1feca83a766f21da599f398430bd386a15a
James Garbutt <43081j@users.noreply.github.com>
2025-01-03 23:23:43 +0000

    chore: drop duplicate fzf bindings

--- zsh/.zsh/keys.zsh
+++ zsh/.zsh/keys.zsh
@@ -1,13 +1,15 @@
-if type "fzf" > /dev/null; then
-	bindkey -M viins '^R' fzf-history-widget
-	bindkey -M vicmd '^R' fzf-history-widget
-	bindkey -M vicmd '^T' fzf-file-widget
-	bindkey -M viins '^T' fzf-file-widget
-	bindkey -M vicmd '\ec' fzf-cd-widget
-else
-	bindkey -M viins '^R' history-incremental-search-backward
-	bindkey -M vicmd '^R' history-incremental-search-backward
-fi
+# Disable this for now since the shipped fzf config should set it up
+# if all is well in a month, bin this
+#if type "fzf" > /dev/null; then
+	#bindkey -M viins '^R' fzf-history-widget
+	#bindkey -M vicmd '^R' fzf-history-widget
+	#bindkey -M vicmd '^T' fzf-file-widget
+	#bindkey -M viins '^T' fzf-file-widget
+	#bindkey -M vicmd '\ec' fzf-cd-widget
+#else
+	#bindkey -M viins '^R' history-incremental-search-backward
+	#bindkey -M vicmd '^R' history-incremental-search-backward
+#fi
 bindkey -M viins "^[[3~" delete-char
 bindkey -M viins "^[[1~" vi-beginning-of-line
 bindkey -M viins "^[[4~" vi-end-of-line
