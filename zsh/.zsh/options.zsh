52c04a160ecf0ef68f22a39875d57f6a9485d5bb
James Garbutt <43081j@users.noreply.github.com>
2026-06-08 14:39:03 +0100

    chore: bump vim plugins

--- zsh/.zsh/options.zsh
+++ zsh/.zsh/options.zsh
@@ -7,3 +7,4 @@ DOTNET_CLI_TELEMETRY_OPTOUT=1
 export BAT_THEME='Dracula'
 export FZF_DEFAULT_COMMAND='fd --type f --strip-cwd-prefix --hidden'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+export EDITOR='nvim'
